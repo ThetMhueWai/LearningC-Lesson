6e60c865ec47283f884649383fecc7c5fd0b6f64
Thet Mhue Wai <waimhuethet@gmail.com>
2024-06-01 05:26:05 +0630

    message change

--- SampleProject/SampleProject/Form1.Designer.cs
+++ SampleProject/SampleProject/Form1.Designer.cs
@@ -30,12 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lblAdminRegister = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bthClear = new System.Windows.Forms.Button();
+            this.btnAdminRegister = new System.Windows.Forms.Button();
+            this.txtaAddress = new System.Windows.Forms.TextBox();
+            this.txtaPassword = new System.Windows.Forms.TextBox();
+            this.txtaPhone = new System.Windows.Forms.TextBox();
+            this.txtaEmail = new System.Windows.Forms.TextBox();
+            this.txtaName = new System.Windows.Forms.TextBox();
+            this.txtAID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAdminRegister = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,13 +60,166 @@
             this.panel1.Size = new System.Drawing.Size(284, 362);
             this.panel1.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 359);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.bthClear);
+            this.panel2.Controls.Add(this.btnAdminRegister);
+            this.panel2.Controls.Add(this.txtaAddress);
+            this.panel2.Controls.Add(this.txtaPassword);
+            this.panel2.Controls.Add(this.txtaPhone);
+            this.panel2.Controls.Add(this.txtaEmail);
+            this.panel2.Controls.Add(this.txtaName);
+            this.panel2.Controls.Add(this.txtAID);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.lblAdminRegister);
             this.panel2.Location = new System.Drawing.Point(291, 1);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(297, 362);
             this.panel2.TabIndex = 1;
+            // 
+            // bthClear
+            // 
+            this.bthClear.Location = new System.Drawing.Point(167, 313);
+            this.bthClear.Name = "bthClear";
+            this.bthClear.Size = new System.Drawing.Size(75, 23);
+            this.bthClear.TabIndex = 14;
+            this.bthClear.Text = "Clear";
+            this.bthClear.UseVisualStyleBackColor = true;
+            this.bthClear.Click += new System.EventHandler(this.bthClear_Click);
+            // 
+            // btnAdminRegister
+            // 
+            this.btnAdminRegister.Location = new System.Drawing.Point(75, 314);
+            this.btnAdminRegister.Name = "btnAdminRegister";
+            this.btnAdminRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnAdminRegister.TabIndex = 13;
+            this.btnAdminRegister.Text = "Register";
+            this.btnAdminRegister.UseVisualStyleBackColor = true;
+            this.btnAdminRegister.Click += new System.EventHandler(this.btnAdminRegister_Click);
+            // 
+            // txtaAddress
+            // 
+            this.txtaAddress.Location = new System.Drawing.Point(167, 264);
+            this.txtaAddress.Name = "txtaAddress";
+            this.txtaAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtaAddress.TabIndex = 12;
+            // 
+            // txtaPassword
+            // 
+            this.txtaPassword.Location = new System.Drawing.Point(167, 223);
+            this.txtaPassword.Name = "txtaPassword";
+            this.txtaPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtaPassword.TabIndex = 11;
+            // 
+            // txtaPhone
+            // 
+            this.txtaPhone.Location = new System.Drawing.Point(167, 182);
+            this.txtaPhone.Name = "txtaPhone";
+            this.txtaPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtaPhone.TabIndex = 10;
+            // 
+            // txtaEmail
+            // 
+            this.txtaEmail.Location = new System.Drawing.Point(167, 143);
+            this.txtaEmail.Name = "txtaEmail";
+            this.txtaEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtaEmail.TabIndex = 9;
+            // 
+            // txtaName
+            // 
+            this.txtaName.Location = new System.Drawing.Point(167, 104);
+            this.txtaName.Name = "txtaName";
+            this.txtaName.Size = new System.Drawing.Size(100, 20);
+            this.txtaName.TabIndex = 8;
+            // 
+            // txtAID
+            // 
+            this.txtAID.Location = new System.Drawing.Point(167, 64);
+            this.txtAID.Name = "txtAID";
+            this.txtAID.Size = new System.Drawing.Size(100, 20);
+            this.txtAID.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(39, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "AdminPhone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(39, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "AdminAddress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(39, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "AdminPassword";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(39, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "AdminEmail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(39, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AdminName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(39, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AdminID";
             // 
             // lblAdminRegister
             // 
@@ -65,16 +232,6 @@
             this.lblAdminRegister.TabIndex = 0;
             this.lblAdminRegister.Text = "Admin Register Here";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(281, 359);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -84,10 +241,11 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -98,6 +256,20 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lblAdminRegister;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bthClear;
+        private System.Windows.Forms.Button btnAdminRegister;
+        private System.Windows.Forms.TextBox txtaAddress;
+        private System.Windows.Forms.TextBox txtaPassword;
+        private System.Windows.Forms.TextBox txtaPhone;
+        private System.Windows.Forms.TextBox txtaEmail;
+        private System.Windows.Forms.TextBox txtaName;
+        private System.Windows.Forms.TextBox txtAID;
     }
 }
 
--- SampleProject/SampleProject/Form1.cs
+++ SampleProject/SampleProject/Form1.cs
@@ -12,9 +12,112 @@ namespace SampleProject
 {
     public partial class Form1 : Form
     {
+        LibraryDatasetTableAdapters.stafftbTableAdapter staff= new LibraryDatasetTableAdapters.stafftbTableAdapter();
+        DataTable staffdta = new DataTable();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Clearall() 
+        {
+            txtaName.Text = "";
+            txtaAddress.Text = "";
+            txtaEmail.Text = "";
+            txtaPassword.Text = "";
+            txtaPhone.Text = "";
+        }
+        public void staffAutoID()
+        {
+            staffdta = staff.GetData();
+
+            if (staffdta.Rows.Count == 0)
+            {
+                txtAID.Text = "S001";
+            }
+            else
+            {
+                int size = staffdta.Rows.Count - 1;
+                string oldid = staffdta.Rows[size][0].ToString();
+                int newid = Convert.ToInt32(oldid.Substring(1, 3));
+
+                if(newid>=1 && newid<9)
+                {
+                    txtAID.Text = "S00" + (newid + 1);
+                }
+                else if (newid >= 9 && newid < 99)
+                {
+                    txtAID.Text = "S0" + (newid + 1);
+                }
+                else if(newid >= 99 && newid < 999)
+                {
+                    txtAID.Text = "S" + (newid + 1);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            staffAutoID();
+        }
+
+        private void bthClear_Click(object sender, EventArgs e)
+        {
+            Clearall();
+        }
+
+        private void btnAdminRegister_Click(object sender, EventArgs e)
+        {
+            String password = txtaPassword.Text;
+            if (txtaName.Text == "")
+            {
+                MessageBox.Show("Fill Admin Name!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaName.Focus();
+            }
+            else if (txtaAddress.Text == "")
+            {
+                MessageBox.Show("Fill Admin Address!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtaEmail.Text == "")
+            {
+                MessageBox.Show("Fill Admin Email!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaEmail.Focus();
+            }
+            else if (txtaPassword.Text == "")
+            {
+                MessageBox.Show("Fill Admin Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPassword.Focus();
+            }
+            else if (txtaPhone.Text == "")
+            {
+                MessageBox.Show("Fill Admin Phone Number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPhone.Focus();
+            }
+            else if(txtaPassword.Text.Length >= 8 || txtaPassword.Text.Length < 16)
+            {
+                MessageBox.Show("Please Vaild Password Length, Should enter between 8 and 16", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPassword.Focus();
+            }
+            else if (!password.Any(char.IsUpper))
+            {
+                MessageBox.Show("Please should include Upper Character", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPassword.Focus();
+            }
+            else if (!password.Any(char.IsLower))
+            {
+                MessageBox.Show("Please should include lower Character", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPassword.Focus();
+            }
+            else if (!password.Any(char.IsDigit))
+            {
+                MessageBox.Show("Please should include digit Character", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaPassword.Focus();
+            }
+            else
+            { 
+                
+            }
         }
     }
 }
